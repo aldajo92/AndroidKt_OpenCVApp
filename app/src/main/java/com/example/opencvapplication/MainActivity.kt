c4de0f010775d8c93ea83be58399bcce532be5b9
Alejandro Gomez <alejodiscovery20@gmail.com>
2022-08-23 17:05:08 -0500

    added opencv module

--- app/src/main/java/com/example/opencvapplication/MainActivity.kt
+++ app/src/main/java/com/example/opencvapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.opencvapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,24 +10,32 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.opencvapplication.ui.theme.OpenCVApplicationTheme
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : ComponentActivity() {
+
+    init {
+        if (!OpenCVLoader.initDebug())
+            Log.d("ERROR", "Unable to load OpenCV")
+        else
+            Log.d("SUCCESS", "OpenCV loaded")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
         setContent {
-//            OpenCVApplicationTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
+            OpenCVApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
         }
     }
 }
@@ -34,12 +43,4 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    OpenCVApplicationTheme {
-        Greeting("Android")
-    }
 }
