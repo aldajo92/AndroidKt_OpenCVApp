318f02f72b4e4b8406be288d3d73f4508c3521d2
Alejandro Gomez <alejodiscovery20@gmail.com>
2022-08-23 17:23:32 -0500

    added camera preview

--- app/src/main/java/com/example/opencvapplication/MainActivity.kt
+++ app/src/main/java/com/example/opencvapplication/MainActivity.kt
@@ -1,46 +1,67 @@
 package com.example.opencvapplication
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.example.opencvapplication.ui.theme.OpenCVApplicationTheme
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.opencv.android.OpenCVLoader
 
 class MainActivity : ComponentActivity() {
 
     init {
         if (!OpenCVLoader.initDebug())
-            Log.d("ERROR", "Unable to load OpenCV")
+            Log.d("aldajo", "Unable to load OpenCV")
         else
-            Log.d("SUCCESS", "OpenCV loaded")
+            Log.d("aldajo", "OpenCV loaded")
+    }
+
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Log.i("aldajo", "Permission granted")
+            shouldShowCamera.value = true
+        } else {
+            Log.i("aldajo", "Permission denied")
+        }
+    }
+
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("aldajo", "Permission previously granted")
+                shouldShowCamera.value = true
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> Log.i("aldajo", "Show camera permissions dialog")
+
+            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         setContent {
-            OpenCVApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            if (shouldShowCamera.value) {
+                CameraView()
             }
         }
+        requestCameraPermission()
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
