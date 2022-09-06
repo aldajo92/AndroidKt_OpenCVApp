fec8a5e9d6408bf9f20e2bf039c091cb33ee9524
Alejandro Gomez <alejodiscovery20@gmail.com>
2022-09-06 17:45:59 -0400

    removed no needed files

--- app/src/main/java/com/aldajo92/opencvapplication/MainActivity.kt
+++ app/src/main/java/com/aldajo92/opencvapplication/MainActivity.kt
@@ -8,8 +8,6 @@ import com.aldajo92.scancv.DocScanCV2Activity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityMainBinding
-//    private lateinit var cameraExecutor: ExecutorService
-//    private var ivBitmap: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,111 +17,6 @@ class MainActivity : AppCompatActivity() {
         viewBinding.buttonCamera.setOnClickListener {
             DocScanCV2Activity.openCamera(this)
         }
-
-//        // Request camera permissions
-//        if (allPermissionsGranted()) {
-//            startCamera()
-//        } else {
-//            ActivityCompat.requestPermissions(
-//                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-//        }
-//
-//        ivBitmap = viewBinding.ivBitmap
-//
-//        cameraExecutor = Executors.newSingleThreadExecutor()
-
     }
 
-//    external fun processFrame(matAddr: Long)
-//
-//    private fun startCamera() {
-//        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-//
-//        cameraProviderFuture.addListener({
-//            // Used to bind the lifecycle of cameras to the lifecycle owner
-//            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-//
-//            // Preview
-//            val preview = Preview.Builder()
-//                .build()
-//                .also {
-//                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
-//                }
-//
-//            val imageAnalyzer = ImageAnalysis.Builder()
-//                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
-//                .build()
-//                .also {
-//                    it.setAnalyzer(cameraExecutor, OpenCVAnalyzer { bitmap ->
-//                        CoroutineScope(Dispatchers.Main).launch {
-//                            ivBitmap?.setImageBitmap(bitmap)
-//                        }
-//                    })
-//                }
-//
-//            try {
-//                // Unbind use cases before rebinding
-//                cameraProvider.unbindAll()
-//
-//                // Bind use cases to camera
-//                cameraProvider.bindToLifecycle(
-//                    this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalyzer)
-//
-//            } catch(exc: Exception) {
-//                Log.e(TAG, "Use case binding failed", exc)
-//            }
-//
-//        }, ContextCompat.getMainExecutor(this))
-//    }
-//
-//    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-//        ContextCompat.checkSelfPermission(
-//            baseContext, it) == PackageManager.PERMISSION_GRANTED
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        cameraExecutor.shutdown()
-//    }
-//
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-//            if (allPermissionsGranted()) {
-//                startCamera()
-//            } else {
-//                Toast.makeText(this,
-//                    "Permissions not granted by the user.",
-//                    Toast.LENGTH_SHORT).show()
-//                finish()
-//            }
-//        }
-//    }
-//
-//    companion object {
-//        init {
-//            if (!OpenCVLoader.initDebug())
-//                Log.d("aldajo", "Unable to load OpenCV")
-//            else
-//                Log.d("aldajo", "OpenCV loaded")
-//
-//            System.loadLibrary("native-lib")
-//        }
-//        private const val TAG = "CameraXApp"
-//        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-//        private const val REQUEST_CODE_PERMISSIONS = 10
-//        private val REQUIRED_PERMISSIONS =
-//            mutableListOf (
-//                Manifest.permission.CAMERA,
-//                Manifest.permission.RECORD_AUDIO
-//            ).apply {
-//                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-//                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                }
-//            }.toTypedArray()
-//    }
 }
